8bf1030e2b296b42f63e7e959c9b1f890821c8d3
杰出咸鱼 <18234312027@163.com>
2026-07-12 20:32:50 +0800

    fix(app): load .env via python-dotenv

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,10 @@
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from backend.app.api import router
+
+load_dotenv()
 
 app = FastAPI(title="王凡周报生成器")
 app.add_middleware(
